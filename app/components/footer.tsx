d50cd8992cddddc374baeb4a849c110b8c00f1e7
Baranpas Jesse <baranpasjesse@Baranpass-MacBook-Air.local>
2025-06-03 10:00:35 -0500

    Numeric copyright symbol

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-zinc-600/20 text-white p-5">
       <div className="flex items-center justify-around">
-        <p className="text-sm">© 2023 Trina's Website</p>
+        <p className="text-sm">&#169; 2023 Trina&apos;s Website</p>
         <ul className="flex space-x-4">
           <li>
             <a href="/privacy" className="hover:underline">
